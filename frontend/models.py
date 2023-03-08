b7cb55c4518290a3019340acc9509f42ff5ea7fa
daychtwoseven <chardrichescol@gmail.com>
2023-03-08 14:31:29 +0800

    c

--- frontend/models.py
+++ frontend/models.py
@@ -7,12 +7,3 @@ class Permit(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
-    project_address = models.CharField(max_length=255)
-    project_documents = models.CharField(max_length=255)
-
-
-class PermitFiles(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    permit = models.ForeignKey(Permit, models.RESTRICT)
-    document = models.CharField(max_length=255)
-    date_created = models.DateTimeField(auto_now_add=True)
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,5 @@
 from django.core.files.storage import FileSystemStorage
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import *
 
 
@@ -9,17 +9,10 @@ def index_page(request):
             name = request.POST.get('name')
             email = request.POST.get('email')
             phone_number = request.POST.get('phone_number')
-            project_address = request.POST.get('project_address')
-            project_documents = request.FILES.getlist('project_documents')
-
-            permit = Permit.objects.create(name=name, email=email, phone_number=phone_number, project_address=project_address)
-            for i in project_documents:
-                fs = FileSystemStorage()
-                name = fs.save(i, i)
-                PermitFiles.objects.create(permit=permit, document=fs.url(name))
-
-        for row in PermitFiles.objects.all():
-            print(row.document)
+            permit = Permit.objects.create(name=name, email=email, phone_number=phone_number)
+            if permit:
+                #request.session['statusMsg'] = True
+                return redirect('/#permit-submitter')
         return render(request, 'frontend/views/index.html')
     except Exception as e:
         print(e)
